e9acd8d01d70e7b68b5fe1ce198920ce05719111
ch.park <hyuny0810@fusionsoft.co.kr>
2024-03-26 13:27:57 +0900

    - 최초커밋

---
+++ msa_sample/item-service/src/main/java/org/msa/item/domain/Item.java
@@ -0,0 +1,41 @@
+package org.msa.item.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="TB_ITEM")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+	
+	@Id
+	@Column(name ="ID", length = 10)
+	private String id;
+	
+	@Column(name ="NAME", length = 20)
+	private String name;
+	
+	@Column(name ="DESCRIPTION", length = 200)
+	private String description;
+	
+	@Column(name ="ITEM_TYPE", length = 1)
+	private String itemType;
+	
+	@Column(name ="CNT", length = 10)
+	private long count;
+
+	@Column(name ="REG_DTS", length = 14)
+	private String regDts;
+
+	@Column(name ="UPD_DTS", length = 14)
+	private String updDts;
+	
+}
---
+++ msa_sample/item-service/src/main/java/org/msa/item/dto/ResponseDTO.java
@@ -0,0 +1,15 @@
+package org.msa.item.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDTO {
+	private String code;
+	private String message;
+}
